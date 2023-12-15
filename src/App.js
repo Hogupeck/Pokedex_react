75ee2664f399e61f8eae2bf208ef6ff024f48c27
hogupeck <pecalhugo@gmail.com>
2023-12-15 09:55:18 +0100

    instal_API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,35 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
+// function App() {
+//   return (
+//     <div className="App">
+
+//     </div>
+//   );
+// }
+
+/* lien import API: "https://sailssoftware.com/how-to-integrate-api-in-reactjs-step-by-step-guide/" */
+/* import adapter au projet*/
+const PokemonList = () => {
+  const [Pokemons, setPokemons] = useState([]);
+
+  useEffect(() => {
+    fetch('https://pokedex-api.3rgo.tech/api/pokemon')
+      .then(response => response.json())
+      .then(data => setPokemons(data))
+      .catch(error => console.error('Error fetching pokemon:', error));
+  }, []);
+
   return (
-    <div className="App">
-    
+    <div>
+      {/*Display the setchd user data*/}
+      {Pokemons.map(pokemon => (
+        <div key={pokemon.id}>{pokemon.name}</div>
+      ))}
     </div>
   );
-}
+};
 
-export default App;
+export default PokemonList;
+// export default App;
